4ae928fec842d88b1e6fb676c2f4c1bbbf0924c1
Wertolet9999 <wertolet9999@gmail.com>
2023-01-21 12:31:31 +0200

    add friendListItem and fix requirements

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,10 @@ import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 
-import profile from '../user.json';
-import data from '../data.json';
-import friends from '../friends.json';
-import transactions from '../transactions';
+import profile from '../../data/user.json';
+import data from '../../data/data.json';
+import friends from '../../data/friends.json';
+import transactions from '../../data/transactions.json';
 
 import css from './App.module.css';
 
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,34 +1,15 @@
 import PropTypes from 'prop-types';
+import {FriendListItem} from 'components/FriendListItem/FriendListItem'
 import css from './FriendList.module.css';
 
 export const FriendList = ({ friends }) => {
   return (
     <ul className={css.friendList}>
-      {friends.map(friend => (
-        <li className={css.item} key={friend.id}>
-          <span
-            className={friend.isOnline ? css.statusOnline : css.statusOffline}
-          ></span>
-          <img
-            className={css.avatar}
-            src={friend.avatar}
-            alt={friend.name}
-            width="48"
-          />
-          <p className={css.name}>{friend.name}</p>
-        </li>
-      ))}
+      {friends.map(friend => FriendListItem(friend))}
     </ul>
   );
 };
 
 FriendList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-    })
-  ),
+  friends: PropTypes.array.isRequired,
 };
